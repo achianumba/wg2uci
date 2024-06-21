2c3e2659a8d520bba697cd97e57e1247492edff5
Arinze Chianumba <chianumbaarinze@gmail.com>
2024-06-21 18:31:42 +0100

    feat: generate uci WG peer conf script from wg-quick conf

--- README.md
+++ README.md
@@ -1,3 +1,44 @@
-- Place the `# InterfaceName = foo0` comment before the `[Interface]` header
-- Place the `# Description = bar0` comment before each `[Peer]` header
-- The `*.conf` file's basename (not including the `.conf` extension) must be a valid Linux interface name if the `#InterfaceName` comment isn't specified.
+# wg2uci
+
+wg2uci uses a [wg-quick](https://git.zx2c4.com/wireguard-tools/about/src/man/wg-quick.8) (WireGuard) configuration file to generate a shell script that configures a WireGuard interface and its peers.
+
+## Usage
+
+Download [wg2uci](wg2uci):
+
+```shell
+curl -O https://raw.githubusercontent.com/achianumba/wg2uci/main/wg2uci
+```
+
+Make `wg2uci` executable:
+
+```shell
+chmod +x wg2uci
+```
+
+Generate a UCI interface/network script:
+
+```shell
+./wg2uci <wg0.conf | ./wg0.conf | /etc/wireguard/wg0.conf>
+```
+
+Configure a WireGuard interface/network using the generated script
+
+```
+./config-INTERFACE.sh
+```
+
+## Additional *.conf file fields
+
+`wg2uci` treats the below comments `*.conf` file as config fields instead of comments:
+
+- `# InterfaceName`: A valid UNIX interface name placed above the `[Interface]` header. E.g. `# InterfaceName = wg0`. The `*.conf` file's name is set as the interface's in the absence of the `InterfaceName` comment.
+  
+- `# Description`: A one-word description of a peer placed above a `[Peer]` header. E.g. `# Description = GitHub CDNs`.
+
+
+## Limitations
+
+- Doesn't parse `PostUp` and `PostDown` directives.
+- Doesn't quite handle multiple AllowedIPs when written on multiple lines (yet) instead of as a comma-separated list
+- Doesn't add the interface to a firewall zone
--- wg2uci.sh
+++ wg2uci.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
 INI_FILEPATH=$1
-INI_FILENAME="$(basename $INI_FILEPATH)"
-# INTERFACE_NAME="$(grep -E "^#(\s)?InterfaceName" $INI_FILEPATH | tr -d '[:space:]' | cut -d '=' -f 2)"
-
-
+INI_FILENAME=$(basename "$INI_FILEPATH")
 
 SCRIPT=$(cat << SCRIPT
 #!/bin/sh
@@ -19,17 +16,15 @@ SCRIPT=$(cat << SCRIPT
 \n#
 \n# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-\n\n# Configure Wireguard Interface
+\n\n# Configure the WireGuard interface
 SCRIPT
 )
 
 declare CURRENT_SECTION
-declare DESCRIPTION
+declare PEER_DESCRIPTION
 declare INTERFACE_NAME
-declare INTERFACE_KEY
-declare INTERFACE_ADDRESSES
-declare INTERFACE_PORT
-declare INTERFACE_DNS
+declare PEER_NAME
+declare PEER_NETWORK
 
 while IFS='=' read -r key value || [ -n "$key" ]; do
   # Strip whitespace
@@ -43,6 +38,7 @@ while IFS='=' read -r key value || [ -n "$key" ]; do
   case $key in
     \#InterfaceName)
       INTERFACE_NAME="$value"
+      continue
       ;;
 
     \[Interface\])
@@ -56,13 +52,24 @@ while IFS='=' read -r key value || [ -n "$key" ]; do
       fi
 
       SCRIPT+="\nuci set network.$INTERFACE_NAME.proto='wireguard'"
+      
+      continue
       ;;
     
-    \[Peer\] | \#Description)
+    \#Description)
+      PEER_DESCRIPTION="$value"
+      ;;
+
+    \[Peer\])
       CURRENT_SECTION='Peer'
+      PEER_NAME="wirguard_$INTERFACE_NAME"
+      PEER_NETWORK="set network.@$PEER_NAME[-1]"
+      SCRIPT+="\n\n# Configure a WireGuard peer\nuci add network $PEER_NAME\nuci $PEER_NETWORK.description='$PEER_DESCRIPTION'"
+      continue
       ;;
   esac
 
+  # Parse Interface fields
   if [ "$CURRENT_SECTION" == "Interface" ]; then
     case "$key" in
       PrivateKey)
@@ -82,7 +89,45 @@ while IFS='=' read -r key value || [ -n "$key" ]; do
         ;;
     esac
   fi
+
+  # Parse peer sections
+  if [ "$CURRENT_SECTION" == "Peer" ]; then
+    case "$key" in
+      PublicKey)
+        SCRIPT+="\nuci $PEER_NETWORK.public_key='$value'"
+        ;;
+
+      Endpoint)
+        SCRIPT+="\nuci $PEER_NETWORK.endpoint_host='${value%:*}'"
+        SCRIPT+="\nuci $PEER_NETWORK.endpoint_port='${value#*:}'"
+        ;;
+
+      PersistentKeepalive)
+        SCRIPT+="\nuci $PEER_NETWORK.persistent_keepalive='$value'"
+        ;;
+      
+      AllowedIPs)
+        SCRIPT+="\nuci $PEER_NETWORK.allowed_ips='$(echo $value | tr ',' ' ')'"
+        ;;
+
+      PresharedKey)
+        SCRIPT+="\nuci $PEER_NETWORK.preshared_key='$value'"
+        ;;
+    esac
+  fi
 done < "$INI_FILEPATH"
 
+SCRIPT+=$(cat << SCRIPT
+\n\n# Commit network changes
+\nuci commit network
+
+\n\n# Restart the network and firewall
+\n/etc/init.d/network restart
+\n/etc/init.d/firewall restart
+SCRIPT
+)
+
+SCRIPT_NAME="config-$INTERFACE_NAME.sh"
 # Write script to file
-echo -e $SCRIPT | tee "configure-$INTERFACE_NAME.sh"
+echo -e $SCRIPT | tee "$SCRIPT_NAME"
+chmod +x "$SCRIPT_NAME"
